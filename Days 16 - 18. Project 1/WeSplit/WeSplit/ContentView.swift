ac90255ccb3b81f9b2300bba8f906d919cb02af6
PetroOnishchuk <petroonishchuk@icloud.com>
2020-01-16 21:30:32 +0200

    End in "Creating views in a loop"  Project 1 Part 1. Clean ContentView

--- Days 16 - 18. Project 1/WeSplit/WeSplit/ContentView.swift
+++ Days 16 - 18. Project 1/WeSplit/WeSplit/ContentView.swift
@@ -9,19 +9,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    let students = ["Harry", "Hermione", "Ron"]
-    @State private var selectedStudent = 0
     
     
     var body: some View {
-        VStack {
-            Picker("Select your student", selection: $selectedStudent) {
-                ForEach(0 ..< students.count) {
-                    Text(self.students[$0])
-                }
-            }
-            Text("You chose: Student # \(students[selectedStudent])")
-        }
+        Text("Hello World")
         
     }
 }
