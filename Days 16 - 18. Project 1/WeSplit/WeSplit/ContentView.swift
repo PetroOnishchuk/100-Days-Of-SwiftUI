fae5c7f1a8fafb9a2f94574ce59332aae1c38e3c
PetroOnishchuk <petroonishchuk@icloud.com>
2020-01-17 06:33:05 +0200

    Project 1 Part 2 "Reading text from the user with TextField" completed

--- Days 16 - 18. Project 1/WeSplit/WeSplit/ContentView.swift
+++ Days 16 - 18. Project 1/WeSplit/WeSplit/ContentView.swift
@@ -9,10 +9,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    let tipPercentages = [10, 15, 20, 25, 0]
     
     var body: some View {
-        Text("Hello World")
+        Form {
+            Section {
+                TextField("Amount", text: $checkAmount)
+                    .keyboardType(.decimalPad)
+            }
+            Section {
+                Text("$\(checkAmount)")
+            }
+        }
         
     }
 }
