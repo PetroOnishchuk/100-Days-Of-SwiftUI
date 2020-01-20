005ccf7f0a00c8e66f6ac47c44795f1e019c8563
PetroOnishchuk <petroonishchuk@icloud.com>
2020-01-20 17:09:12 +0200

    Project 2 Part1. Showing alert message

--- Days 20 - 22. Project 2/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ Days 20 - 22. Project 2/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,24 +9,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showingAlert = false
+    
+    
     var body: some View {
-        
-        Button(action: {
-            print("Edit button was tapped")
-        }) {
-            HStack(spacing: 10) {
-                Image(systemName: "pencil")
-                    //.renderingMode(.original)
-                Text("Edit")
-                
-            }
+        Button("Show Alert") {
+            self.showingAlert = true
+            
+        }.alert(isPresented: $showingAlert) { () -> Alert in
+            Alert(title: Text("Hello SwiftUI!"), message: Text("This is some detail message"), dismissButton: .default(Text("OK")))
         }
-          
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
 }
