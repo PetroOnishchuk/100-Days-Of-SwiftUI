03634bcab31e4393ab8acfcc99a053859d875aa2
PetroOnishchuk <petroonishchuk@icloud.com>
2020-01-22 06:50:10 +0200

    pause in Project 3. part 1 1. Custom containers

--- Day 23 - 24. Project 3/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ Day 23 - 24. Project 3/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -44,13 +44,31 @@ struct Watermark: ViewModifier {
     var text: String
     
     func body(content: Content) -> some View {
-        ZStack(alignment: .bottomTrailing) {
-            content
-            Text(text)
-                .font(.caption)
-                .foregroundColor(.white)
-            .padding(5)
-                .background(Color.black)
+        GridStack(rows: 4, columns: 4) { row, col in
+            HStack {
+            Image(systemName: "\(row * 4 + col).circle")
+                Text("R:\(row) C \(col)")}
+        }
+    }
+}
+
+// MARK: GridStack
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    let content: (Int, Int) -> Content
+    
+    var body: some View {
+        VStack {
+            ForEach(0 ..< rows) {
+                row in
+                HStack {
+                    ForEach(0 ..< self.columns) {
+                        column in
+                        self.content(row, column)
+                    }
+                }
+            }
         }
     }
 }
@@ -88,4 +106,6 @@ extension View {
     func watermarked(with text: String) -> some View {
         self.modifier(Watermark(text: text))
     }
+    
+    
 }
