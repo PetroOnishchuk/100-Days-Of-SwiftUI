aa125e96e8898958a5448ca90c915fc515d56a54
PetroOnishchuk <petroonishchuk@icloud.com>
2020-01-22 06:29:38 +0200

    Project 3. Part 1. Custom modifiers.

--- Day 23 - 24. Project 3/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ Day 23 - 24. Project 3/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -8,6 +8,8 @@
 
 import SwiftUI
 
+
+// MARk: CapsuleText View
 struct CapsuleText: View {
     var text: String
     
@@ -22,6 +24,36 @@ struct CapsuleText: View {
 }
 
 
+//MARK: Custom modefiers
+
+struct Title: ViewModifier {
+    
+    
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+        .padding()
+            .background((Color.blue))
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+            
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+            .padding(5)
+                .background(Color.black)
+        }
+    }
+}
 
 
 
@@ -30,10 +62,10 @@ struct ContentView: View {
 
     var body: some View {
         VStack {
-            CapsuleText(text: "First")
-                .foregroundColor(.white)
-            CapsuleText(text: "Second")
-                .foregroundColor(.white)
+            Color.blue
+            .frame(width: 300, height: 300)
+            .watermarked(with: "Hacking with Swift")
+            
         }
     }
 }
@@ -41,5 +73,19 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+// MARK: extension View
+extension View {
+    func titleStyle() -> some View {
+        self.modifier(Title())
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        self.modifier(Watermark(text: text))
     }
 }
