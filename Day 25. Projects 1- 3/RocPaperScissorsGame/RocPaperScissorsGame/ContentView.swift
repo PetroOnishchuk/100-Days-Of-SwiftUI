7275f4baa36a80544e66c2456a4147e77a9b1951
PetroOnishchuk <petroonishchuk@icloud.com>
2020-01-23 21:03:35 +0200

    Create Day 25. Project 1 - 3 "RockPaperScissorsGame"

---
+++ Day 25. Projects 1- 3/RocPaperScissorsGame/RocPaperScissorsGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RocPaperScissorsGame
+//
+//  Created by Petro Onishchuk on 1/23/20.
+//  Copyright © 2020 Petro Onishchuk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
