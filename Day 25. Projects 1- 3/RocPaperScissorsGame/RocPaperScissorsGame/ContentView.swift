414dd946878b9e57b622d3121548ee92d7ab5097
PetroOnishchuk <petroonishchuk@icloud.com>
2020-01-24 17:32:47 +0200

    Day 25.  Project 3. Create DrawImageView, DrawHorisontalText and add logic

--- Day 25. Projects 1- 3/RocPaperScissorsGame/RocPaperScissorsGame/ContentView.swift
+++ Day 25. Projects 1- 3/RocPaperScissorsGame/RocPaperScissorsGame/ContentView.swift
@@ -8,9 +8,121 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct drawText: ViewModifier {
+    let font = Font.system(size: 25, weight: .black, design: .default)
+    
+    func body(content: Content) -> some View {
+        content
+            .font(font)
+        
+    
+        
+    }
+}
+
+
+struct DrawHorisontalText: View {
+    var text: String
+    var textResult: String
+    
+    
     var body: some View {
-        Text("Hello, World!")
+        HStack {
+            Text(text)
+                .modifier(drawText())
+                .foregroundColor(Color.green)
+            
+            Text(textResult)
+                .modifier(drawText())
+                .foregroundColor(Color.red)
+            
+            
+            
+            
+        }
+    }
+}
+
+
+struct DrawImageView: View  {
+    var imageName: String
+    
+    var body: some View {
+        Text("\(imageName)")
+        .padding()
+        .padding()
+            .background(Color.yellow)
+        .cornerRadius(25)
+            .overlay(RoundedRectangle(cornerRadius: 25).stroke(Color.red, lineWidth: 2))
+            .shadow(color: .blue, radius: 3)
+            .font(.largeTitle)
+    }
+}
+
+
+
+
+struct ContentView: View {
+    @State var currentChoise = 0
+    @State var winOrLose = Bool.random()
+    @State var totalScore = 0
+    @State var showingScore = false
+    
+    @State var randomComputeChoise = Int.random(in: 0...2)
+    
+    let possibleMoves = ["🗿", "📃", "✂️"]
+    
+    
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .orange]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+            VStack(spacing: 100) {
+                Text("Rock Paper Scissors")
+                    .modifier(drawText())
+                VStack(spacing: 50) {
+                    
+                    DrawHorisontalText(text: "You score is: ", textResult: "\(totalScore)")
+                    
+                    DrawImageView(imageName: "\(possibleMoves[randomComputeChoise])")
+                    
+                    DrawHorisontalText(text: "You must: ", textResult: winOrLose ? "Win" : "Lose")
+                    
+                    HStack {
+                        ForEach(0 ..< possibleMoves.count) { number in
+                            Button(action: {
+                                self.checkToWin(selectedName: self.possibleMoves[number])
+                                
+                            }) {
+                                DrawImageView(imageName: "\(self.possibleMoves[number])")
+                            }                        }
+                    }
+                }
+                Spacer()
+            }
+        }
+    }
+    
+    func checkToWin(selectedName: String) {
+        guard let index = possibleMoves.firstIndex(where: { return $0 == possibleMoves[randomComputeChoise]}) else { return }
+        let prefixArray = possibleMoves.prefix(upTo: index)
+        let suffixArray = possibleMoves.suffix(from: index)
+        
+        let wrappedArray  = suffixArray + prefixArray
+        
+        guard let computedIndex = wrappedArray.firstIndex(where: { return $0 == possibleMoves[randomComputeChoise]}) else { return }
+        guard let selectedIndex = wrappedArray.firstIndex(where: { return $0 == selectedName }) else { return }
+        
+        if winOrLose {
+            totalScore += computedIndex + 1 == selectedIndex ? 1 : 0
+        } else {
+            totalScore += !(computedIndex + 1 == selectedIndex) ? 1 : 0
+        }
+        
+        self.winOrLose = Bool.random()
+        self.randomComputeChoise = Int.random(in: 0...2)
+        
+        // run new game
     }
 }
 
