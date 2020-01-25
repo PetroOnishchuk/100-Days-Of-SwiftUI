7e1d2b8c7a94563d9abdb2282f1854edbc28a592
PetroOnishchuk <petroonishchuk@icloud.com>
2020-01-25 07:00:28 +0200

    Project 4. BetterResr Days 26 - 28 .  Pause

--- Days 26 - 28 Project 4/BetterRest/BetterRest/ContentView.swift
+++ Days 26 - 28 Project 4/BetterRest/BetterRest/ContentView.swift
@@ -8,9 +8,27 @@
 
 import SwiftUI
 
+
+
+
+
 struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    @State private var wekeUp = Date()
+    
+    // when you create a new Date instance it will be set to the current date and time
+    let now = Date()
+    
+    // create a second Date instance set to one day in seconds from now
+    let tomorrow = Date().addingTimeInterval(86400)
+    
+    // create a range from those two
+    //let range = now ... tomorrow
+    
     var body: some View {
-        Text("Hello, World!")
+        Form {
+            DatePicker("Please enter a date", selection: $wekeUp, displayedComponents: .hourAndMinute)
+        }
     }
 }
 
