bc8a754a68bf5105d657fd02eeb657d7dfe824a4
PetroOnishchuk <petroonishchuk@icloud.com>
2020-01-27 06:26:23 +0200

    Days 29 - 31 . Project 5 "WordScramble" created

---
+++ Days 29 - 31 Project 5/WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Petro Onishchuk on 1/27/20.
+//  Copyright © 2020 Petro Onishchuk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
