47d116930ee7c47423e0302c990c4a5d5a17c5e8
PetroOnishchuk <petroonishchuk@icloud.com>
2020-01-27 16:20:02 +0200

    Project 5 day 29 part 1 . UITextChecker

--- Days 29 - 31 Project 5/WordScramble/WordScramble/ContentView.swift
+++ Days 29 - 31 Project 5/WordScramble/WordScramble/ContentView.swift
@@ -17,17 +17,11 @@ struct ContentView: View {
     let people = ["Finn", "Leia", "Luke", "Rey"]
     
     var body: some View {
-        let input = """
-                    a
-                    b
-                    c
-                    """
-        let letters = input.components(separatedBy: "\n")
-        let letter = letters.randomElement()
-        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
-        
-        
-        
+        let word = "swift"
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelleRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        let allGood = misspelleRange.location == NSNotFound
         
         
         
