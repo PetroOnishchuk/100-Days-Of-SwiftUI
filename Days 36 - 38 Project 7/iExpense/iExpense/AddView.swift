a2da63b072795cd89098cf801aaf986f13146e67
PetroOnishchuk <petroonishchuk@icloud.com>
2020-02-05 16:10:03 +0200

    Project 7 iExpense Making changes permanents with UserDefault Comleted

---
+++ Days 36 - 38 Project 7/iExpense/iExpense/AddView.swift
@@ -0,0 +1,47 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Petro Onishchuk on 2/5/20.
+//  Copyright © 2020 Petro Onishchuk. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = ""
+    
+    static let types = ["Business", "Personal"]
+    
+    @ObservedObject var expenses: Expenses
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                Picker("Type", selection: $type) {
+                    ForEach(Self.types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                TextField("Amount", text: $amount)
+                    .keyboardType(.numberPad)
+            }
+        .navigationBarTitle("Add new expense")
+            .navigationBarItems(trailing: Button("Save") {
+                if let actualAmount = Int(self.amount) {
+                    let item = ExpenseItem(name: self.name, type: self.type, amount: actualAmount)
+                    self.expenses.items.append(item)
+                }
+            })
+        }
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView(expenses: Expenses())
+    }
+}
--- Days 36 - 38 Project 7/iExpense/iExpense/ContentView.swift
+++ Days 36 - 38 Project 7/iExpense/iExpense/ContentView.swift
@@ -8,7 +8,7 @@
 
 import SwiftUI
 
-struct ExpenseItem: Identifiable {
+struct ExpenseItem: Identifiable, Codable {
     let id = UUID()
     let name: String
     let type: String
@@ -17,13 +17,34 @@ struct ExpenseItem: Identifiable {
 }
 
 class Expenses: ObservableObject {
-    @Published var items = [ExpenseItem]()
+    @Published var items = [ExpenseItem]() {
+        didSet {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    
+    init() {
+        if let items = UserDefaults.standard.data(forKey: "Items") {
+            let decoder = JSONDecoder()
+            
+            if let decoded = try? decoder.decode([ExpenseItem].self, from: items) {
+                self.items = decoded
+            }
+        }
+        self.items = []
+    }
 }
 
    
 
 struct ContentView: View {
     @ObservedObject var expenses = Expenses()
+    
+    @State private var showingAddExpense = false
+    
     
     var body: some View {
         NavigationView {
@@ -35,12 +56,13 @@ struct ContentView: View {
         .navigationBarTitle("iExpense")
         .navigationBarItems(trailing:
             Button(action: {
-           let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
-                      self.expenses.items.append(expense)
+                self.showingAddExpense = true
                 
             }){
                 Image(systemName: "plus")
             })
+                .sheet(isPresented: $showingAddExpense) { AddView(expenses: self.expenses)
+            }
             
         }
     }
