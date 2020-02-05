3d6035f6622b14a1c6bb7183b5b97ac2d7bc1392
PetroOnishchuk <petroonishchuk@icloud.com>
2020-02-05 16:37:44 +0200

    Challenge 1 For Project 7 iExpense Done

--- Days 36 - 38 Project 7/iExpense/iExpense/ContentView.swift
+++ Days 36 - 38 Project 7/iExpense/iExpense/ContentView.swift
@@ -64,7 +64,7 @@ struct ContentView: View {
                 }.onDelete(perform: removeItems(at:))
             }
         .navigationBarTitle("iExpense")
-        .navigationBarItems(trailing:
+        .navigationBarItems(leading: EditButton() , trailing:
             Button(action: {
                 self.showingAddExpense = true
                 
