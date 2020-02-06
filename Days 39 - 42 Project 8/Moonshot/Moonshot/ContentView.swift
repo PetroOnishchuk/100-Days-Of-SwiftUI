8c0be87754b685283280831c6d38d8aa3101389b
PetroOnishchuk <petroonishchuk@icloud.com>
2020-02-06 21:05:54 +0200

    Create Project 8 Moonshot Days 39 - 42

---
+++ Days 39 - 42 Project 8/Moonshot/Moonshot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Petro Onishchuk on 2/6/20.
+//  Copyright © 2020 Petro Onishchuk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
