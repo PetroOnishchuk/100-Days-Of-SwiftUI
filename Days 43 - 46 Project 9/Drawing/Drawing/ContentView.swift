da17245222980d77296aeb0befddf625d145431c
PetroOnishchuk <petroonishchuk@icloud.com>
2020-02-11 20:49:09 +0200

    Day 44 Project 9 Enabling high-performance Metal rendering with drawingGroup() Done

--- Days 43 - 46 Project 9/Drawing/Drawing/ContentView.swift
+++ Days 43 - 46 Project 9/Drawing/Drawing/ContentView.swift
@@ -83,19 +83,49 @@ struct Flower: Shape {
     }
 }
 
+struct ColorCyclingCircle: View  {
+    var amount = 0.0
+    var steps = 100
+    
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) {
+                value in
+                Circle()
+                .inset(by: CGFloat(value))
+                .strokeBorder(LinearGradient(gradient: Gradient(colors: [
+                    self.color(for: value, brightness: 1),
+                    self.color(for: value, brightness: 0.5)]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
+            }
+        }
+    .drawingGroup()
+    }
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(self.steps) + self.amount
+        
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+        
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
+
 
 
 
 struct ContentView: View {
+    @State private var colorCycle = 0.0
+    
     
     
     var body: some View {
-//        Text("Hello Word")
-//            .frame(width: 300, height: 300)
-//            .border(ImagePaint(image: Image("dog"),sourceRect: CGRect(x: 0, y: 0.25, width: 1, height: 0.5), scale: 0.1), width: 30)
-        Capsule()
-            .strokeBorder(ImagePaint(image: Image("dog"), scale: 0.1), lineWidth: 20)
-            .frame(width: 300, height: 200)
+        VStack {
+            ColorCyclingCircle(amount: self.colorCycle)
+                .frame(width: 300, height: 300)
+            
+            Slider(value: $colorCycle)
+        }
         
     }
 }
