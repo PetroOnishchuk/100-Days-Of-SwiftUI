8ff9e9789f826b38738cfa609ab53f0fdeac6d85
PetroOnishchuk <petroonishchuk@icloud.com>
2020-02-11 17:40:18 +0200

    Day 44 Project 9 Creative border and fills using ImagePaint

--- Days 43 - 46 Project 9/Drawing/Drawing/ContentView.swift
+++ Days 43 - 46 Project 9/Drawing/Drawing/ContentView.swift
@@ -87,23 +87,15 @@ struct Flower: Shape {
 
 
 struct ContentView: View {
-    @State private var petalOffset = -20.0
-    @State private var petalWidth = 100.0
+    
     
     var body: some View {
-        VStack {
-            Flower(petalOffSet: petalOffset, petaWidth: petalWidth)
-                .fill(Color.red, style: FillStyle(eoFill: true))
-            
-            Text("Offset")
-            Slider(value: $petalOffset, in: -40...40)
-                .padding([.horizontal, .bottom])
-            
-            Text("Width")
-            Slider(value: $petalWidth, in: 0...100)
-                .padding(.horizontal)
-            
-        }
+//        Text("Hello Word")
+//            .frame(width: 300, height: 300)
+//            .border(ImagePaint(image: Image("dog"),sourceRect: CGRect(x: 0, y: 0.25, width: 1, height: 0.5), scale: 0.1), width: 30)
+        Capsule()
+            .strokeBorder(ImagePaint(image: Image("dog"), scale: 0.1), lineWidth: 20)
+            .frame(width: 300, height: 200)
         
     }
 }
