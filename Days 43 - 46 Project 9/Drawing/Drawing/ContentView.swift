b9d28a96b6817dd685237e4b30827b606a5dc067
PetroOnishchuk <petroonishchuk@icloud.com>
2020-02-12 07:21:19 +0200

    Day 45 Project 9 Animating complex shapes with AnimatablePair. Done

--- Days 43 - 46 Project 9/Drawing/Drawing/ContentView.swift
+++ Days 43 - 46 Project 9/Drawing/Drawing/ContentView.swift
@@ -171,6 +171,8 @@ struct ContentView3: View {
     
 }
 
+
+//Day 45. Part 3.Animating simple shapes with animatableData
 struct Trapezoid: Shape {
     var insetAmount: CGFloat
     
@@ -193,17 +195,57 @@ struct Trapezoid: Shape {
 }
 
 
+// Day 45. Part 3. Animating complex shapes with AnimatablePair
+struct Checkerboard: Shape {
+    var rows: Int
+    var columns: Int
+    
+    public var animatableData: AnimatablePair<Double, Double> {
+        get {
+            AnimatablePair(Double(rows), Double(columns))
+        }
+        
+        set {
+            self.rows = Int(newValue.first)
+            self.columns = Int(newValue.second)
+        }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        // figure out big each row/column needs to be
+        let rowSize = rect.height / CGFloat(rows)
+        let columnSize = rect.width / CGFloat(columns)
+        
+        // loop over all rows and coluns, making alternating squares colored
+        for row in 0..<rows {
+            for column in 0..<columns {
+                if (row + columns).isMultiple(of: 2) {
+                    // this square should be colored, add a rectengle here
+                    let startX = columnSize * CGFloat(column)
+                    let startY = rowSize * CGFloat(row)
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
+            }
+        }
+        return path
+    }
+}
+
 struct ContentView: View {
-    @State private var insetAmount: CGFloat = 50
+   @State private var rows = 4
+    @State private var columns = 4
     
     
     
     var body: some View {
-       Trapezoid(insetAmount: insetAmount)
-        .frame(width: 200, height: 100)
+       Checkerboard(rows: rows, columns: columns)
         .onTapGesture {
-            withAnimation {
-            self.insetAmount = CGFloat.random(in: 10...90)
+            withAnimation(.linear(duration: 3)) {
+                self.rows = 8
+                self.columns = 16
             }
         }
     }
