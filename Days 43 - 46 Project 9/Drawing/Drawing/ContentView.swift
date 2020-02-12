5c5dfe75333208b1b8c667af62459c044e4863a8
PetroOnishchuk <petroonishchuk@icloud.com>
2020-02-12 20:43:58 +0200

    Day 45. Project 9 . Creating a spirograph with SwiftUI . Done

--- Days 43 - 46 Project 9/Drawing/Drawing/ContentView.swift
+++ Days 43 - 46 Project 9/Drawing/Drawing/ContentView.swift
@@ -234,18 +234,96 @@ struct Checkerboard: Shape {
     }
 }
 
+
+// Day 45. Creating a spinograph  with SwiftUI
+
+struct Spirograph: Shape {
+    let innerRadius: Int
+    let outerRadius: Int
+    let distance: Int
+    let amount: CGFloat
+    
+    func gcd(_ a: Int, _ b: Int) -> Int {
+        var a = a
+        var b = b
+        
+        while b != 0 {
+            let temp = b
+            b = a % b
+            a = temp
+        }
+        return a
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        let division = gcd(innerRadius, outerRadius)
+        let outerRadius = CGFloat(self.outerRadius)
+        let innerRadius = CGFloat(self.innerRadius)
+        let distance = CGFloat(self.distance)
+        let difference = innerRadius - outerRadius
+        let endPoint = ceil(2 * CGFloat.pi * outerRadius / CGFloat(division)) * amount
+        
+        var path = Path()
+        
+        for theta in stride(from: 0, through: endPoint, by: 0.01) {
+            var x = difference * cos(theta) + distance * cos(difference / outerRadius * theta)
+            var y = difference * sin(theta) - distance * sin(difference / outerRadius * theta)
+            
+            x += rect.width / 2
+            y += rect.height / 2
+            
+            if theta == 0 {
+                path.move(to: CGPoint(x: x, y: y))
+            } else {
+                path.addLine(to: CGPoint(x: x, y: y))
+            }
+        }
+        return path
+    }
+}
+
+
+
 struct ContentView: View {
-   @State private var rows = 4
-    @State private var columns = 4
+    @State private var innerRadius = 125.0
+    @State private var outherRadius = 75.0
+    @State private var distance = 25.0
+    @State private var amount: CGFloat = 1.0
+    @State private var hue = 0.6
     
     
+ 
     
     var body: some View {
-       Checkerboard(rows: rows, columns: columns)
-        .onTapGesture {
-            withAnimation(.linear(duration: 3)) {
-                self.rows = 8
-                self.columns = 16
+        VStack(spacing: 0) {
+            Spacer()
+            
+            Spirograph(innerRadius: Int(innerRadius), outerRadius: Int(outherRadius), distance: Int(distance), amount: amount)
+            .stroke(Color(hue: hue, saturation: 1, brightness: 1), lineWidth: 1)
+                .frame(width: 300, height: 300)
+            
+            Spacer()
+            
+            Group {
+                Text("Inner radius: \(Int(innerRadius))")
+                Slider(value: $innerRadius, in: 10...150, step: 1)
+                    .padding([.horizontal, .bottom])
+                
+                Text("Outer radius: \(Int(outherRadius))")
+                Slider(value: $outherRadius, in: 10...150, step: 1)
+                .padding([.horizontal, .bottom])
+                
+                Text("Distance: \(Int(distance))")
+                         Slider(value: $distance, in: 10...150, step: 1)
+                         .padding([.horizontal, .bottom])
+                
+                Text("Amount: \(amount, specifier: "%.2f")")
+                         Slider(value: $amount)
+                         .padding([.horizontal, .bottom])
+                
+                Text("Color")
+                Slider(value: $hue)
+                    .padding(.horizontal)
             }
         }
     }
