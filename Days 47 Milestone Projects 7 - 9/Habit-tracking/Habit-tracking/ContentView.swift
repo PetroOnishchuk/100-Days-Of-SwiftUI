72c89c6702a2ce449794c87f91947b689cb42700
PetroOnishchuk <petroonishchuk@icloud.com>
2020-02-16 16:15:56 +0200

    Days 47 Milestone Projects 7 - 9. Habit-tracking start

---
+++ Days 47 Milestone Projects 7 - 9/Habit-tracking/Habit-tracking/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Habit-tracking
+//
+//  Created by Petro Onishchuk on 2/16/20.
+//  Copyright © 2020 Petro Onishchuk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
