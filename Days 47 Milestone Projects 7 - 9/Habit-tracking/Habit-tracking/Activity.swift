0ec6aac37962192c38c40049d1ca4735ca9dc0b5
PetroOnishchuk <petroonishchuk@icloud.com>
2020-02-18 22:04:10 +0200

    Day 49. Project 10., part one "Sending and receiving Codable data with URLSession and SwiftUI"

--- Days 47 Milestone Projects 7 - 9/Habit-tracking/Habit-tracking/Activity.swift
+++ Days 47 Milestone Projects 7 - 9/Habit-tracking/Habit-tracking/Activity.swift
@@ -9,8 +9,16 @@
 import Foundation
 import SwiftUI
 
+// didSet with @Published don't work (yet) (iOS 13.4, Version 11.4 beta (11N111s))
 class Activities: ObservableObject {
-    @Published var items = [Activity]()
+    @Published var items: [Activity]{
+        didSet {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
     
     func saveActivities() {
         let encoder = JSONEncoder()
---
+++ Days 49 - 52 Project 10/CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -0,0 +1,91 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Petro Onishchuk on 2/18/20.
+//  Copyright © 2020 Petro Onishchuk. All rights reserved.
+//
+
+import SwiftUI
+
+class User: ObservableObject, Codable {
+    @Published var name = "Paul Hudson"
+    
+    
+    enum CodingKeys: CodingKey {
+        case name
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container  = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+    }
+}
+
+struct Response: Codable {
+    var results: [Result]
+    
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
+
+struct ContentView: View {
+    @State private var results = [Result]()
+
+    var body: some View {
+        List(results, id: \.trackId) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                Text(item.collectionName)
+            }
+        }.onAppear(perform: loadData)
+    }
+    
+    func loadData() {
+        // step 1
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        // step 2
+        let request = URLRequest(url: url)
+        
+        // step 3
+        
+        URLSession.shared.dataTask(with: request) {
+            data, response, error in
+            // step 4
+            if let data = data {
+                if let decodedResponse  = try? JSONDecoder().decode(Response.self, from: data) {
+                    // we have good data - go back to the main  thread
+                    DispatchQueue.main.async {
+                        // update our UI
+                        print("\(String(data: data, encoding: .utf8))")
+                        self.results = decodedResponse.results
+                    }
+                    // everthing is good, so we can exit
+                    return
+                }
+            }
+            // if we're still here it means there was a problem
+            print("Fetch failse: \(error?.localizedDescription ?? "Unknow error")")
+        }.resume()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
