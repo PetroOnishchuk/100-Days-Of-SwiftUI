5477569fa52e0f00fec82cb618d84964a69a896d
PetroOnishchuk <petroonishchuk@icloud.com>
2020-02-18 22:33:05 +0200

    Day 49. Project 10., part one "Validatiing and disabling forms" Done

--- Days 49 - 52 Project 10/CupcakeCorner/CupcakeCorner/ContentView.swift
+++ Days 49 - 52 Project 10/CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -39,7 +39,7 @@ struct Result: Codable {
 }
 
 
-struct ContentView: View {
+struct ContentViewPrevious: View {
     @State private var results = [Result]()
 
     var body: some View {
@@ -83,6 +83,34 @@ struct ContentView: View {
         }.resume()
     }
 }
+
+
+struct ContentView: View {
+    @State private var username = ""
+    @State private var email = ""
+
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
+    }
+    var body: some View {
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
+            }
+            
+            Section {
+                Button("Create account") {
+                    print("Creating account")
+                }
+            }.disabled(disableForm)
+        }
+    }
+    
+    
+}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
