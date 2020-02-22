9311ee380a330506006d912d82189129d6a52e53
PetroOnishchuk <petroonishchuk@icloud.com>
2020-02-22 20:45:21 +0200

    Day 55. Project 10, part 3. Deleting from a Core Data fetch request. Done

--- Days 53 - 56 Project 11/Bookworm/Bookworm/ContentView.swift
+++ Days 53 - 56 Project 11/Bookworm/Bookworm/ContentView.swift
@@ -36,6 +36,7 @@ struct ContentView: View {
                         }
                     }
                 }
+            .onDelete(perform: deleteBooks(at:))
             }
                 
                 
@@ -43,15 +44,27 @@ struct ContentView: View {
                 
                 
             .navigationBarTitle("Bookworm")
-                .navigationBarItems(trailing: Button(action: {
-                    self.showingAddScreen.toggle()
-                }, label: {
-                    Image(systemName: "plus")
-                }))
+            .navigationBarItems(leading: EditButton(), trailing: Button(action: {
+                self.showingAddScreen.toggle()
+            }, label: {
+                Image(systemName: "plus")
+            }))
                 .sheet(isPresented: $showingAddScreen) {
                     AddBookView().environment(\.managedObjectContext, self.moc)
             }
         }
+    }
+    func deleteBooks(at offsets: IndexSet) {
+        for offset in offsets {
+            // find this book in our fetch request
+            let book = books[offset]
+            
+            // delete it from the context
+            moc.delete(book)
+        }
+        
+        // save the context
+        try? moc.save()
     }
     
     
