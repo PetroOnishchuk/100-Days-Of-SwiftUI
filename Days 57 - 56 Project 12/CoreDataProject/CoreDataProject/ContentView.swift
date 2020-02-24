2297077cd0a92f7b7e5773c80b822c0488eecff0
PetroOnishchuk <petroonishchuk@icloud.com>
2020-02-24 08:29:49 +0200

    Day 57. Project 12, part 1. Conditional saving of NSManagedObjectContext

--- Days 57 - 56 Project 12/CoreDataProject/CoreDataProject/ContentView.swift
+++ Days 57 - 56 Project 12/CoreDataProject/CoreDataProject/ContentView.swift
@@ -8,16 +8,19 @@
 
 import SwiftUI
 
+
+
 struct Student: Hashable {
     let name: String
 }
 
 struct ContentView: View {
-    let students = [Student(name: "Harry Potter"), Student(name: "Hermione Granger")]
+    @Environment(\.managedObjectContext) var moc
     var body: some View {
-        List(students, id: \.self) {
-            student in
-            Text(student.name)
+        Button("Save") {
+            if self.moc.hasChanges {
+            try? self.moc.save()
+            }
         }
     }
 }
