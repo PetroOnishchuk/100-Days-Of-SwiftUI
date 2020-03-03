a7d5201c1beb6a99b2a1e756d1cbb0ba10452e2d
PetroOnishchuk <petroonishchuk@icloud.com>
2020-03-03 08:11:22 +0200

    Day 61. Paused in projects 10 -12 with CoreData

---
+++ Milestone Projects 10 - 12/CoreDataVariant/UsersListCoreData/User+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  User+CoreDataClass.swift
+//  UsersListCoreData
+//
+//  Created by Petro Onishchuk on 3/3/20.
+//  Copyright © 2020 Petro Onishchuk. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(User)
+public class User: NSManagedObject {
+
+}
---
+++ Milestone Projects 10 - 12/CoreDataVariant/UsersListCoreData/User+CoreDataProperties.swift
@@ -0,0 +1,44 @@
+//
+//  User+CoreDataProperties.swift
+//  UsersListCoreData
+//
+//  Created by Petro Onishchuk on 3/3/20.
+//  Copyright © 2020 Petro Onishchuk. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension User {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<User> {
+        return NSFetchRequest<User>(entityName: "User")
+    }
+
+    @NSManaged public var id: String?
+    @NSManaged public var name: String?
+    @NSManaged public var company: String?
+    @NSManaged public var age: Int16
+    @NSManaged public var isActive: Bool
+    @NSManaged public var friends: NSSet?
+
+}
+
+// MARK: Generated accessors for friends
+extension User {
+
+    @objc(addFriendsObject:)
+    @NSManaged public func addToFriends(_ value: User)
+
+    @objc(removeFriendsObject:)
+    @NSManaged public func removeFromFriends(_ value: User)
+
+    @objc(addFriends:)
+    @NSManaged public func addToFriends(_ values: NSSet)
+
+    @objc(removeFriends:)
+    @NSManaged public func removeFromFriends(_ values: NSSet)
+
+}
