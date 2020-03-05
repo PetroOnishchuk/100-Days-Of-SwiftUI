577fda1f8597d14fbe73015f1eb41821f791a7fc
PetroOnishchuk <petroonishchuk@icloud.com>
2020-03-05 22:43:41 +0200

    Day 61. UsersListV2CodeData . Completed

---
+++ Milestone Projects 10 - 12/CoreDataVariant/UsersListV2CodeData/UsersListV2CodeData/LoadedUsers.swift
@@ -0,0 +1,89 @@
+//
+//  LoadedUsers.swift
+//  UsersListV2CodeData
+//
+//  Created by Petro Onishchuk on 3/4/20.
+//  Copyright © 2020 Petro Onishchuk. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import CoreData
+
+class Users {
+    
+    static func loadDataToCD(moc: NSManagedObjectContext) {
+        
+        loadDataFromJSON { (users) in
+            DispatchQueue.main.async {
+                
+                
+                var tempUsers = [User]()
+                
+                for user in users {
+                    let newUser = User(context: moc)
+                    newUser.name = user.name
+                    newUser.id = user.id
+                    newUser.company = user.company
+                    newUser.isActive = user.isActive
+                    newUser.age = Int16(user.age)
+                    tempUsers.append(newUser)
+                }
+                
+                
+                for i in 0..<users.count {
+                    
+                    for friend in users[i].friends {
+                        
+                        let newFriend = Friend(context: moc)
+                        newFriend.name = friend.name
+                        newFriend.id = friend.id
+                        
+                        tempUsers[i].addToFriends(newFriend)
+                        
+                    }
+                    
+                    
+                }
+                
+                do {
+                     try moc.save()
+                } catch let error {
+                    print("Could not save data: \(error.localizedDescription)")
+                }
+                
+            }
+            
+        }
+    }
+    
+    static func loadDataFromJSON(completion: @escaping ([LoadedUser]) -> ()) {
+        
+        let stringURL = "https://www.hackingwithswift.com/samples/friendface.json"
+        
+        guard let url = URL(string: stringURL) else { return }
+        
+        let request = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: request) {
+            data, response, error in
+            
+            guard let data = data else {
+                print("No data in response \(error?.localizedDescription ?? "No data response")")
+                return
+            }
+            
+            if let decoderLoadedUser = try? JSONDecoder().decode([LoadedUser].self, from: data) {
+                
+                completion(decoderLoadedUser)
+                
+            }
+        }.resume()
+        
+        
+    }
+}
+
+
+
+
