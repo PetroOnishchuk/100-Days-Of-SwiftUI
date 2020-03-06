6732149a8c1d7e2870bda4c5223b8e83c7adc46d
PetroOnishchuk <petroonishchuk@icloud.com>
2020-03-06 21:40:06 +0200

    Day 62. Project 13, part 1. Create project Instafilter

---
+++ Days 62 - 67 Project 13/Instafilter/Instafilter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Petro Onishchuk on 3/6/20.
+//  Copyright © 2020 Petro Onishchuk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
