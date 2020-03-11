191a55e7e5d3db73ed86112c98596fb9f1d2e0b9
PetroOnishchuk <petroonishchuk@icloud.com>
2020-03-11 17:59:00 +0200

    Starts Project 14. Day 68

---
+++ Days 68 - 73. Project 14/BucketList/BucketList/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by Petro Onishchuk on 3/11/20.
+//  Copyright © 2020 Petro Onishchuk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("Adding conformance to Comparable for custom types")) {
+                    NavigationLink(destination: AddingConformanceToComparableForCustomTypes()) {
+                        Text("AddingConformanceToComparableForCustomTypes")
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Days 68 - 73. Project 14/BucketList/BucketList/Views/AddingConformanceToComparableForCustomTypes.swift
@@ -0,0 +1,21 @@
+//
+//  AddingConformanceToComparableForCustomTypes.swift
+//  BucketList
+//
+//  Created by Petro Onishchuk on 3/11/20.
+//  Copyright © 2020 Petro Onishchuk. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddingConformanceToComparableForCustomTypes: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddingConformanceToComparableForCustomTypes_Previews: PreviewProvider {
+    static var previews: some View {
+        AddingConformanceToComparableForCustomTypes()
+    }
+}
