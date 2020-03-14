d99885686984772b842deaed8de81c2d928921ef
PetroOnishchuk <petroonishchuk@icloud.com>
2020-03-14 22:06:15 +0200

    Day 71. Project 14. Part 4. Done

--- Days 68 - 73. Project 14/BucketList/BucketList/Views/EditView.swift
+++ Days 68 - 73. Project 14/BucketList/BucketList/Views/EditView.swift
@@ -30,7 +30,7 @@ struct EditView: View {
                             Text(page.title)
                                 .font(.headline)
                             + Text(": ") +
-                            Text("Page description here")
+                                Text(page.description)
                             .italic()
                             
                         }
@@ -68,7 +68,7 @@ struct EditView: View {
                 
                 if let items = try? decoder.decode(Result.self, from: data) {
                     // success - convert the array values to our pages array
-                    self.pages = Array(items.query.pages.values)
+                    self.pages = Array(items.query.pages.values).sorted()
                     self.loadingState = .loaded
                     return
                 }
--- Days 68 - 73. Project 14/BucketList/BucketList/Views/Result.swift
+++ Days 68 - 73. Project 14/BucketList/BucketList/Views/Result.swift
@@ -16,8 +16,16 @@ struct Query: Codable {
     let pages: [Int: Page]
 }
 
-struct Page: Codable {
+struct Page: Codable, Comparable {
     let pageid: Int
     let title: String
     let terms: [String: [String]]?
+    
+    var description: String {
+        terms?["description"]?.first ?? "No futher information"
+    }
+    
+    static func < (lhs: Page, rhs: Page) -> Bool {
+        lhs.title < rhs.title
+    }
 }
