e0ad40f7c2e7699f66029947c1184b21f2c76cf7
PetroOnishchuk <petroonishchuk@icloud.com>
2020-04-04 17:04:56 +0300

    Update Day 77 - 78 Projects 13 - 15. Create projects HotProstects Days 79 - 85 Project 16

---
+++ Days 79 - 85 Project 16/HotProspects/HotProspects/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Petro Onishchuk on 4/4/20.
+//  Copyright © 2020 Petro Onishchuk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
