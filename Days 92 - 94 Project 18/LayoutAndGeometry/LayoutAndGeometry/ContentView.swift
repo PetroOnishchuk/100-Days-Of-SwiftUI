3b41f309d0cc034c6a3dcd4b1e2c4682adafaaac
PetroOnishchuk <petroonishchuk@icloud.com>
2020-04-20 18:10:24 +0300

    Day 92. Project 18, part 1 "Alignment and alignment guides" Done

--- Days 92 - 94 Project 18/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ Days 92 - 94 Project 18/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -10,7 +10,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        //V.1
+//        HStack(alignment: .lastTextBaseline) {
+//            Text("Live")
+//                .font(.caption)
+//            Text("long")
+//            Text("and")
+//                .font(.title)
+//            Text("poster")
+//                .font(.largeTitle)
+//        }
+        //V.2
+//        VStack(alignment: .leading, spacing: nil) {
+//            Text("Hello, world!")
+//                .alignmentGuide(.leading) { (d ) -> CGFloat in
+//                    d[.trailing]
+//            }
+//            Text("Hello is a longer line of text")
+//        }
+//        .background(Color.red)
+//        .frame(width: 400, height: 400)
+//        .background(Color.blue)
+        
+        VStack(alignment: .leading, spacing: nil) {
+            ForEach(0..<10) {
+                position in
+                Text("Number \(position)")
+                    .alignmentGuide(.leading) { (_) -> CGFloat in
+                        CGFloat(position) * -10
+                }
+            }
+        
+        }
+        .background(Color.red)
+        .frame(width: 400, height: 400)
+        .background(Color.blue)
     }
 }
 
