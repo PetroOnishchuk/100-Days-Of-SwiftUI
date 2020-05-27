b5b0048a81621558f364594b5afc5c88178c0bbe
PetroOnishchuk <petroonishchuk@icloud.com>
2020-05-27 23:02:41 +0300

    update

--- Days 47 Milestone Projects 7 - 9/Habit-tracking/Habit-tracking/Activity.swift
+++ Days 47 Milestone Projects 7 - 9/Habit-tracking/Habit-tracking/Activity.swift
@@ -55,3 +55,33 @@ struct Activity: Identifiable, Codable {
         return lhs.id == rhs.id
     }
 }
+
+/*
+ In our example we have struct Activity.
+ Our struct activity has a forth properties
+ 
+ let id = UUID()
+ let name: String
+ let description: String
+ var completedTimes: Int
+ 
+ We wont to compare one Activity object and another Activity object, and say, This is two Activity object is EQUAL or NOT EQUAL
+ 
+ static func in our example have access to the Object and all properties  in our object.
+ 
+ And we must say Xcode what properties in two object, he must compare.
+ 
+ And through our static func we take "id" properties from two Activity object and
+ if firtsActiviti.id (lhs.id)  the same secondsActivity.id(rhs.id)
+ if the same return true if not return false.
+ 
+ If we wont to compare a simple
+ let firstAge = 10
+ let secondAge = 20
+ 
+ I hope, i explained.
+ For ourself, search Compare custom Object in Swift.
+ 
+ 
+ 
+ */
